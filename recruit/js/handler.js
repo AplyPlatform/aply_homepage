cfa36a3d6d09d75844758ed6b919f15822f3dced
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2021-08-07 20:08:38 +0900

    recruit 요청 - get > post 로 변경

--- recruit/js/handler.js
+++ recruit/js/handler.js
@@ -70,6 +70,7 @@ function sendApplicationData(form_id, token)
 	var ref = $('<input type="hidden" value="' + document.referrer + '" name="ref">');
 	$(form_id).append(ref);
 
+	/*
 	var sed = "?form_kind=recruit&form_name="
 							+ encodeURIComponent(form_name)
 							+ "&form_phone=" + encodeURIComponent(form_phone)
@@ -77,13 +78,18 @@ function sendApplicationData(form_id, token)
 							+ "&form_token=" + encodeURIComponent(token)
 							+ "&min_type=" + encodeURIComponent(min_type)
 							+ "&ref=" + encodeURIComponent(document.referrer);
-	//var sed = new FormData($(form_id)[0]);
+	*/
+	var sed = new FormData($(form_id)[0]);
 
 
 	$.ajax({
 		url: 'https://duni.io/handler/handler.php' + sed,
 		crossDomain: true,
 		dataType: "json",
+		data:sed,
+		enctype: 'multipart/form-data', // 필수
+		processData: false,
+    contentType: false,
     cache: false,
 		success: function (data) {
 			if (data.result == "success") {
