2c485a6ea8e7e5b005bc864639b07c84dd9dd60e
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2022-06-30 19:16:59 +0900

    로고 경로 수정

--- symbol/js/aply_sign.js
+++ symbol/js/aply_sign.js
@@ -44,7 +44,7 @@ function processChange() {
           			+ curEmail + '" target="_blank" style="color: rgb(153, 153, 153);">' + curEmail + '</a><br><br><br>'
             		+ '&nbsp;&nbsp;<em><strong><font size="2">APLY Inc.</font></strong></em><br>&nbsp;&nbsp;<font color="#8e7cc3"><strong>T</strong></font>&nbsp;+82-2-6956-0801&nbsp;&nbsp;'
             		+ '|&nbsp;&nbsp;<font color="#8e7cc3"><strong>H</strong></font>&nbsp;&nbsp;<a href="http://www.aply.biz/" target="_blank"><font color="#999999">www.aply.biz</font></a><br>'
-            		+ '<strong><font color="#8e7cc3">&nbsp;&nbsp;Office</font></strong><br>&nbsp;&nbsp;02751 | 5F 17-5,&nbsp;Hwarang-ro 11-gil, Seongbuk-gu,<br>&nbsp;&nbsp;Seoul, Republic of Korea<br><br>&nbsp;&nbsp;<a href="https://aply.biz/" target="_blank"><img src="https://aply.biz/assets/images/logo.png" width="96" height="30"/></a>'
+            		+ '<strong><font color="#8e7cc3">&nbsp;&nbsp;Office</font></strong><br>&nbsp;&nbsp;02751 | 5F 17-5,&nbsp;Hwarang-ro 11-gil, Seongbuk-gu,<br>&nbsp;&nbsp;Seoul, Republic of Korea<br><br>&nbsp;&nbsp;<a href="https://home.aply.biz/" target="_blank"><img src="https://home.aply.biz/assets/images/logo.png" width="96" height="30"/></a>'
             		+ '<br><br><font color="#ddd" size="1">____________________________________________</font></font>';
 	
 	//alert(sigStr);
