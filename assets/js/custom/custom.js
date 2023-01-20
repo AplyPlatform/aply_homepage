2d510a6dbef0c8a295d79eaa00fcce0a0c56e89b
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2023-01-20 20:44:26 +0900

    하단 리스트의 블로그 주소 수정

--- assets/js/custom/custom.js
+++ assets/js/custom/custom.js
@@ -29,7 +29,7 @@
         ["aplx", "https://aplx.aply.biz"],
         ["dkdk", "https://dkdk.io"],
         ["drdr", "https://drdr.io"],
-        ["blog", "https://blog.naver.com/duniplatform"],
+        ["blog", "https://blog.naver.com/PostList.naver?blogId=jebo78&categoryNo=38&from=postList"],
         ["catchme", "https://catchme.aply.biz"],
         ["kiosk", "https://kiosk.aply.biz"],
         ["polaris", "https://polarisconsulting.modoo.at"]
