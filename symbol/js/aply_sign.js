1898716dcf8d2d9c4d0cc7cb988778c668d403d0
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2023-08-23 11:32:17 +0900

    이메일 서명의 aplx, aqr 링크 주소 변경

--- symbol/js/aply_sign.js
+++ symbol/js/aply_sign.js
@@ -56,8 +56,8 @@ function processChange() {
             		+ '<br>&nbsp;&nbsp;<font color="#8e7cc3"><strong>O</strong></font> 10915 <font color="#8e7cc3">|</font> 6F, 24-21,&nbsp;Geumbit-ro, Paju-si,<br>&nbsp;&nbsp;Gyeonggi-do, Republic of Korea<br>&nbsp;&nbsp;'
 					+ '<table border="0" cellpadding="0" cellspacing="0" width="220px"><tr><td width="4px"></td><td width="110px" align="left" valign="center"><a href="https://aply.biz/" target="_blank"><img src="https://home.aply.biz/assets/images/logo.png" width="96" height="30"/></a></td>'
 					+ '<td width="1" bgcolor="#8e7cc3"></td>'
-					+ '<td align="right" valign="center" width="52px"><a href="https://aplx.aply.biz/" target="_blank"><img src="https://home.aply.biz/symbol/images/aplx_og_image.png" width="30" height="30" alt="증강현실 기반의 홍보/전시/커뮤니케이션 플랫폼"/></a></td>'
-					+ '<td align="center" valign="center" width="53px"><a href="https://aplx.aply.biz/aqr" target="_blank"><img src="https://home.aply.biz/symbol/images/aqr_squre2.png" width="30" height="30" alt="간편 송금 지원 서비스"/></a>'
+					+ '<td align="right" valign="center" width="52px"><a href="https://aplx.link" target="_blank"><img src="https://home.aply.biz/symbol/images/aplx_og_image.png" width="30" height="30" alt="증강현실 기반의 홍보/전시/커뮤니케이션 플랫폼"/></a></td>'
+					+ '<td align="center" valign="center" width="53px"><a href="https://aqr.aplx.link" target="_blank"><img src="https://home.aply.biz/symbol/images/aqr_squre2.png" width="30" height="30" alt="간편 송금 지원 서비스"/></a>'
 					+ '</td></tr></table>'
             		+ '<br><br><font color="#ddd" size="1">____________________________________________</font></div>';
 	
