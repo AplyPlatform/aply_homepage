fd1ceac85bd9265b552371e337f428f123ad5b5e
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2024-12-02 13:09:32 +0900

    서명 하단의 서비스 이미지 크기 수정

--- symbol/js/aply_sign.js
+++ symbol/js/aply_sign.js
@@ -57,11 +57,14 @@ function processChange() {
 					<br>
 					<br>
 					<br>
-					<table border="0" cellpadding="0" cellspacing="0" width="240px"><tr><td>						
-						<a href="https://aqr.aplx.link" target="_blank"><img src="images/aqr_per.png" style="margin-right:44px;width:35px;" title="간편 송금을 지원하는 Link-in-bio 서비스"></a>
-						<a href="https://aqr-m.aplx.link" target="_blank"><img src="images/aqrm_per.png" style="margin-right:42px;width:36px;" title="QR코드 기반의 Mass production 관리 솔루션"></a>
-						<a href="https://aplx.link" target="_blank"><img src="images/aplx_per.png" style="width:40px;" title="증강현실 기반의 홍보/전시/커뮤니케이션 플랫폼"></a>
+					<table border="0" cellpadding="0" cellspacing="0" width="240px">
+					<tr>
+					<td>						
+						<a href="https://aqr.aplx.link" target="_blank"><img src="https://home.aply.biz/symbol/images/aqr_per.png" style="margin-right:44px;width:35px;" title="간편 송금을 지원하는 Link-in-bio 서비스"></a>
+						<a href="https://aqr-m.aplx.link" target="_blank"><img src="https://home.aply.biz/symbol/images/aqrm_per.png" style="margin-right:42px;width:36px;" title="QR코드 기반의 Mass production 관리 솔루션"></a>
+						<a href="https://aplx.link" target="_blank"><img src="https://home.aply.biz/symbol/images/aplx_per.png" style="width:40px;" title="증강현실 기반의 홍보/전시/커뮤니케이션 플랫폼"></a>						
 					</td>
+					<tr>					
 					</tr>
 					</table>
 					<br><br>
