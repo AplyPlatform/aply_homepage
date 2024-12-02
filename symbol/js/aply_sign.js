606d1b2289b53106a7f07152a94d764e64a5c59a
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2024-12-02 11:53:34 +0900

    서명 디자인 수정

--- symbol/js/aply_sign.js
+++ symbol/js/aply_sign.js
@@ -40,22 +40,47 @@ function setSign() {
 
 
 function processChange() {	
-	var sigStr = '<div style="font-size: x-small; font-family:나눔고딕,NanumGothic,Sans-serif;" color="#777"><br><font color="#ddd" size="1">____________________________________________</font><br><br><br>'
-					+ '&nbsp;&nbsp;<font size="2"><strong style="font-family:나눔고딕,NanumGothic,Sans-serif">' + curName + ' (' + curEngName + ')</strong> ' + curTitle + '</font>'
-					+ '<br>&nbsp;&nbsp;<strong style="color:#000"><b>M</b></strong>&nbsp;'
-          			+ curNumber + '&nbsp;&nbsp;<font color="#000">|</font>&nbsp;&nbsp<strong style="color:#000"><b>E</b></strong>&nbsp;&nbsp;<a href="mailto:'
-          			+ curEmail + '" target="_blank" style="color:#777">' + curEmail + '</a><br><br><br>'
-					+ '&nbsp;&nbsp;<font color="#6668a6">ADVANCED PLATFORM TO FLY</font><br>'
-            		+ '&nbsp;&nbsp;<strong><font size="2" color="#000">주식회사 어플라이 APLY Inc.</font></strong><br>&nbsp;&nbsp;<strong style="color:#000"><b>T</b></strong>&nbsp;+82-2-6956-0801&nbsp;&nbsp;'
-            		+ '<font color="#000">|</font>&nbsp;&nbsp;<strong style="color:#000"><b>H</b></strong>&nbsp;&nbsp;<a href="http://aply.biz/" target="_blank" style="color:#777">www.aply.biz</font></a><br>'
-            		+ '<br>&nbsp;&nbsp;<strong style="color:#000"><b>O</b></strong> 10915 <font color="#000">|</font> 6F, 24-21,&nbsp;Geumbit-ro, Paju-si,<br>&nbsp;&nbsp;Gyeonggi-do, Republic of Korea<br>&nbsp;&nbsp;'
-					+ '<table border="0" cellpadding="0" cellspacing="0" width="239px"><tr><td width="4px" rowspan="2"></td>'
-					+ '<td width="104px" align="left" valign="center" rowspan="2"><a href="https://aply.biz/" target="_blank"><img src="https://home.aply.biz/assets/images/logo.png" height="25px"/></a></td>'
-					+ '<td width="1px" rowspan="2" bgcolor="#eee"></td>'
-					+ '<td width="65px" height="33px" align="center" valign="center" ><a href="https://aqr.aplx.link" target="_blank"><img src="https://home.aply.biz/symbol/images/aqr_squre2.png" width="25px" height="25px" alt="간편 송금 지원 서비스"/></a></td>'
-					+ '<td width="65px" align="center" valign="center" rowspan="2"><a href="https://aplx.link/to/aply" target="_blank"><img src="https://home.aply.biz/symbol/images/aqr_qr.png" width="58px" height="58px" alt="APLY Link-in-bio"/></a></td></tr>'
-					+ '<tr><td width="65px" height="33px" align="center" valign="center" ><a href="https://aplx.link" target="_blank"><img src="https://home.aply.biz/symbol/images/aplx_og_image.png" width="25px" height="25px" alt="증강현실 기반의 홍보/전시/커뮤니케이션 플랫폼"/></a></td></tr></table>'
-            		+ '<br><br><font color="#ddd" size="1">____________________________________________</font></div>';
+	var sigStr = `<div style="font-size: x-small; font-family:나눔고딕,NanumGothic,Sans-serif;" color="#777"><br><font color="#ddd" size="1">____________________________________________</font><br><br><br>
+					&nbsp;&nbsp;<font size="2"><strong style="font-family:나눔고딕,NanumGothic,Sans-serif">` + curName + ` (` + curEngName + `)</strong> ` + curTitle + `</font>
+					<br>&nbsp;&nbsp;<strong style="color:#000"><b>M</b></strong>&nbsp;`
+          			+ curNumber + `&nbsp;&nbsp;<font color="#000">|</font>&nbsp;&nbsp<strong style="color:#000"><b>E</b></strong>&nbsp;&nbsp;<a href="mailto:`
+          			+ curEmail + `" target="_blank" style="color:#777">` + curEmail + `</a><br><br>					
+					<br>&nbsp;&nbsp;<a href="https://aply.biz/" target="_blank"><img src="https://home.aply.biz/assets/images/logo.png" style="height:25px" title="Advanced platform to fly"/></a>
+					<br>&nbsp;&nbsp;<font color="#6668a6">ADVANCED PLATFORM TO FLY</font>
+					
+            		<br>&nbsp;&nbsp;<strong><font size="3" color="#6668a6">주식회사 어플라이 APLY Inc.</font></strong>
+					<br>&nbsp;&nbsp;<img src="https://home.aply.biz/assets/images/aqr_square.png" width="16px" height="16px">&nbsp;&nbsp;<a href="http://aplx.link/to/aply" target="_blank" style="color:#777">aplx.link/to/aply</font></a>
+					<br>
+					<br>&nbsp;&nbsp;<strong style="color:#000"><b>T</b></strong>&nbsp;+82-2-6956-0801&nbsp;&nbsp;<font color="#000">|</font>&nbsp;&nbsp;
+					<strong style="color:#000"><b>H</b></strong>&nbsp;&nbsp;<a href="http://aply.biz/" target="_blank" style="color:#000">www.aply.biz</font></a>
+					<br>&nbsp;&nbsp;<strong style="color:#000"><b>O</b></strong> 10915 &nbsp;<font color="#000">|</font>&nbsp; 6F, 24-21,&nbsp;Geumbit-ro, Paju-si,<br>&nbsp;&nbsp;Gyeonggi-do, Republic of Korea
+					<br>
+					<br>
+					<table border="0" cellpadding="0" cellspacing="0" width="240px"><tr><td>						
+						<a href="https://aqr.aplx.link" target="_blank"><img src="https://home.aply.biz/assets/images/aqr_per.png" style="margin-right:26px;width:56px;" title="간편 송금을 지원하는 Link-in-bio 서비스"></a>
+						<a href="https://aqr-m.aplx.link" target="_blank"><img src="https://home.aply.biz/assets/images/aqrm_per.png" style="margin-right:24px;width:48px;" title="QR코드 기반의 Mass production 관리 솔루션"></a>
+						<a href="https://aplx.link" target="_blank"><img src="https://home.aply.biz/assets/images/aplx_per.png" style="width:48px;" title="증강현실 기반의 홍보/전시/커뮤니케이션 플랫폼"></a>
+					</td>
+					</tr>
+					</table>
+					<br><br>
+					<font color="#ddd" size="1">____________________________________________</font>
+					</div>`;					
+	
+					/*
+
+					<a href="https://home.aply.biz" target="_blank"><img src="https://home.aply.biz/symbol/images/aply_per.png" style="margin-right:16px;width:56px;"></a>
+						<a href="https://aqr.aplx.link" target="_blank"><img src="https://home.aply.biz/symbol/images/aqr_per.png" style="margin-right:18px;width:56px;"></a>
+						<a href="https://aqr-m.aplx.link" target="_blank"><img src="https://home.aply.biz/symbol/images/aqrm_per.png" style="width:48px;"></a>
+
+
+	<td width="104px" align="left" valign="center" rowspan="2"><a href="https://aply.biz/" target="_blank"><img src="https://home.aply.biz/assets/images/logo.png" height="25px"/></a></td>
+	<td width="1px" rowspan="2" bgcolor="#eee"></td>
+	<td width="65px" height="33px" align="center" valign="center" ><a href="https://aqr.aplx.link" target="_blank"><img src="https://home.aply.biz/symbol/images/aqr_squre2.png" width="25px" height="25px" alt="간편 송금 지원 서비스"/></a></td>
+	<td width="65px" align="center" valign="center" rowspan="2"><a href="https://aplx.link/to/aply" target="_blank"><img src="https://home.aply.biz/symbol/images/aqr_qr.png" width="58px" height="58px" alt="APLY Link-in-bio"/></a></td></tr>
+	<tr><td width="65px" height="33px" align="center" valign="center" ><a href="https://aplx.link" target="_blank"><img src="https://home.aply.biz/symbol/images/aplx_og_image.png" width="25px" height="25px" alt="증강현실 기반의 홍보/전시/커뮤니케이션 플랫폼"/></a></td></tr></table>
+	<br><br><font color="#ddd" size="1">____________________________________________</font></div>`;
+	*/
 	
 
 	$("#sign_field").html(sigStr);	
