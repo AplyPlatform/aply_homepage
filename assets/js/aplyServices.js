7fe4967e270b3a339c6f74e12db7866cd3d57222
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2025-07-05 12:27:42 +0900

    turnstile 초기화 방법 개선

--- assets/js/aplyServices.js
+++ assets/js/aplyServices.js
@@ -44,6 +44,8 @@ $(function () {
 });
 
 function setAAPICaptchaInfo() {
+	if (!turnstile || turnstile == null || turnstile === "undefined" || (typeof turnstile === "undefined")) return;
+
 	turnstile.ready(function () {
 		AAPI_isRecaptchaInit = true;
 	});
